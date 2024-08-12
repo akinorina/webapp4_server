7eff5bcf1cf532d2c680aee4c74f15e97cd5ce03
Akinori Nakata <akinori.nakata@bright-century.com>
2024-08-12 17:51:56 +0900

    Users Validation 実装

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import configuration from './config/configuration';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const options = { cors: configuration().app.cors };
   const app = await NestFactory.create(AppModule, options);
   app.setGlobalPrefix('/api');
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(configuration().app.port);
 }
 bootstrap();
