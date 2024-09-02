9c00c7a6bcaca0477cbd4da1373fe030b9d6140b
Akinori Nakata <akinori.nakata@bright-century.com>
2024-09-02 11:55:00 +0900

    blog を作成者のみ閲覧・編集可に更新

--- src/blogs/blogs.module.ts
+++ src/blogs/blogs.module.ts
@@ -3,10 +3,11 @@ import { DatabaseModule } from 'src/database/database.module';
 import { BlogsService } from './blogs.service';
 import { BlogsController } from './blogs.controller';
 import { blogProviders } from './entities/blog.providers';
+import { userProviders } from 'src/users/entities/user.providers';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [BlogsController],
-  providers: [...blogProviders, BlogsService],
+  providers: [...blogProviders, BlogsService, ...userProviders],
 })
 export class BlogsModule {}
