d01051eed254fc18c00621bd9e385728791872a6
Akinori Nakata <akinori.nakata@bright-century.com>
2025-02-13 18:30:24 +0900

    Google Auth コメントを修正

--- src/auth/google-auth.service.ts
+++ src/auth/google-auth.service.ts
@@ -16,21 +16,25 @@ export class GoogleAuthService {
       return 'no_user_from_google';
     }
 
-    // search the user on DB.
+    // emailによるユーザー検索
     let targetUser = await this.usersService.findOneByEmail(req.user.email);
     if (!targetUser) {
+      // email見つからない => ユーザー新樹登録: DBにユーザーを作成
+
+      // 削除済みを含めてemailによるユーザー検索
       const targetUserDeleted = await this.usersService.findOneByEmail(
         req.user.email,
         true,
       );
       if (targetUserDeleted) {
+        // 一度不正で削除処理したユーザーの場合
         throw new HttpException(
           'このアカウントでの登録はできません' + ' : ' + req.user.email,
           HttpStatus.BAD_REQUEST,
         );
       }
 
-      // create new user from google-auth data.
+      // google-auth によるサイン-アップ、ユーザー情報をDBに新樹作成
       const newUserDto = new CreateUserDto();
       newUserDto.username = req.user.username;
       newUserDto.familyname = req.user.familyname;
@@ -40,7 +44,7 @@ export class GoogleAuthService {
       targetUser = await this.usersService.create(newUserDto);
     }
 
-    // create and return the access token.
+    // access token 作成
     const roles = targetUser.roles.map((item) => {
       return item.name;
     });
